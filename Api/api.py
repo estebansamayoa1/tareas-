13dda25af49a51de0bbb6058d4862f7aa8203198
estebansamayoa1 <61565155+estebansamayoa1@users.noreply.github.com>
2021-02-09 18:27:36 -0600

    Add files via upload

---
+++ Api/api.py
@@ -0,0 +1,62 @@
+from flask import Flask 
+from random import randint
+import string
+import csv
+
+
+app=Flask(__name__)
+
+with open('datos.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+
+def llenar(key):
+   
+    values=[]
+    for i in range(0,key):
+        values.append((randint(0,100)))
+    return values
+
+def linear_search(valores, value):
+    if value in valores:
+        return True
+    else: 
+        return False
+       
+
+def binary_search(arr, l, r, x): 
+    x=int(x)
+    while l <= r: 
+        mid = l + (r - l) // 2
+        if arr[mid] == x: 
+            return True
+        elif arr[mid] < x: 
+            l = mid + 1
+  
+        else: 
+            r = mid - 1
+    return False
+
+
+@app.route('/')
+def home():
+    return ("Bienvenido\n")
+
+@app.route('/linear/<int:largo>/<int:key>/')
+def linear(largo, key):
+    valores=llenar(largo)
+    if (linear_search(valores, key))==True:
+        return ("El número si está en el arreglo\n")
+    else:
+        return ("El número no está en el arreglo\n")
+
+@app.route('/binary/<int:largo>/<int:key>/')
+def binary(largo, key):
+    valores=llenar(largo)
+    if (binary_search(valores, 0, len(valores)-1, key))==True:
+        return ("El número si está en el arreglo\n")
+    else:
+        return ("El número no está en el arreglo\n")
+    
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
